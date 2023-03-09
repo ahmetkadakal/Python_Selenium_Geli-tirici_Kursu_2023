518e25454258abec199fcffd507bb4af522cda30
ahmetkadakal <ahmetkadakal464@gmail.com>
2023-03-09 21:08:27 +0300

    ilk gün odevi

---
+++ Python_Selenium_Geliştirici_Kursu_Ödevleri/dersBir/kodlamaio_veriturleri_ifelse.py
@@ -0,0 +1,18 @@
+"""
+ben direk giriş yapılan sayfadan örnek vermek istiyorum 
+    -kullanıcı adı kısmı = String bir değişkene bağlı
+    -şifre kısmıda = String bir değişkene bağlı
+    -giriş yap kısmı ise bir if else karar mekanizmasıyla yapılabilir
+    aşşağıda örneklendireceğim gibi
+
+Mesela:
+    if(kullaniciAdi == girilenKullaniciAdi and sifre == girilenSifre):
+        print("başarılı giriş")
+    elif(kullaniciAdi != girilenKullaniciAdi and sifre != girilenSifre):
+        print("kullanici adi veya sifre yanlis")
+    else:
+        print("boş veri")
+
+gibi bir karar mekanizması kurulmuş olabilir
+
+"""
---
+++ Python_Selenium_Geliştirici_Kursu_Ödevleri/dersBir/veri_tipleri.py
@@ -0,0 +1,28 @@
+"""
+Pythonda temel olarak bakılırsa 2 tür değişken tipi görürüz bunlar :
+    -String : Metinsel ifadeler için kullanılan veri tipi 
+    örn:
+        adSoyad = "Ahmet Kadakal"
+    şeklinde yapılan atamalar için kullanılırlar 
+
+    -number : Bu tür ise sayısal değerleri tutarlar ve kendi içinde 2 gruptan oluşurlar
+    tam sayılar ve ondalıklı sayılar.
+    --integer : int olarak kullanımı gerçekleştirilir tam sayıları tutar
+        örn:
+        sayiBir = 10
+    --float : float şeklinde kullanılır ondalıklı sayıları tutat
+        örn:
+        sayiBir = 10.5
+
+yukarda bahsedilenler temel veri türleriydi bunlar dışında list(diziler) tuple'ler(içeriyi değiştirilemeyen diziler)
+ve dictonery(sözlük) yapılarıda bulunmaktadır 
+
+listeler genel olarak birden fazla veriyi saklamakla ilgilenirken dictoneryler key ve value değerler tutarak 
+geröek hayyataki sözlüklere benzer bir yapıdadırlar
+    
+
+
+
+
+"""
+
